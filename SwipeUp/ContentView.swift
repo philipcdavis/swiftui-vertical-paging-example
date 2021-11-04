38854f406259517135205478331f78811f7111a9
Philip Davis <philipcdavis11@gmail.com>
2021-11-03 21:20:13 -0700

    Initial Commit

---
+++ SwipeUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwipeUp
+//
+//  Created by Philip Davis on 11/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwipeUp/SwipeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeUpApp.swift
+//  SwipeUp
+//
+//  Created by Philip Davis on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwipeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
